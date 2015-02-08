42366f1ff7222d742e7cc297a4fc49e107d27bb8
JPWS2013 <justin.poh@students.olin.edu>
2015-02-07 21:50:34 -0500

    Added loader matlab script for loading data

---
+++ loader.m
@@ -0,0 +1,23 @@
+%Loading Script
+%Loads all data from folder into MATLAB workspace for processing
+
+%NOTE: Check that the file path in "FolderName" is correctly specified 
+%      before running script
+
+clear all
+
+FolderName='Sub015_Exported_MATLAB/*mat';%Sets the file path
+%/*.mat part specifies only to obtain file names with .mat in them
+
+FileList=dir(FolderName); %Retrieves all folder names
+
+NumFiles=size(FileList); %Determines the number of .mat files to load
+
+for i = 1:NumFiles(1) %Iterates through each the length of FileList
+    name=FileList(i).name; %Retrieves the file name of the file as a string
+    filepath= strcat('Sub015_Exported_MATLAB/', name); %Concatenates the file name to form the full file path
+    load(filepath) %Loads the data file
+
+end
+
+clear FileList filepath FolderName i name NumFiles %Clears all variables created in this script
